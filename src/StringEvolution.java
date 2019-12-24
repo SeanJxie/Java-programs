124c7f5f1f17b3a6663c35ac2b1c33665cc75024
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-12-24 11:20:18 -0500

    optimize

--- src/StringEvolution.java
+++ src/StringEvolution.java
@@ -2,15 +2,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 /*
-A program that takes an input string, creates a random-character string of the same length, then proceeds to "evolve" the random
-string back to the original input string.
+A program that takes an input string, creates a random-character string of the same length, then proceeds to "evolve"
+the random string back to the original input string.
 */
 
 public class StringEvolution {
     public static void main(String[] args) {
         System.out.print("Enter a target string: ");
         Scanner input = new Scanner(System.in);
-        String targetString = input.nextLine().lower();
+        String targetString = input.nextLine().toLowerCase();
 
         char[] alphabet = {
                 'a', 'b', 'c', 'd', 'e', 'f', 'g',
@@ -23,9 +23,9 @@ public class StringEvolution {
 
         // Create random string with same length as input.
         StringBuilder buildString = new StringBuilder();
+        Random randomIndex = new Random();
         int length = targetString.length();
         for (int c = 0; c < length; c++) {
-            Random randomIndex = new Random();
             buildString.append(alphabet[randomIndex.nextInt(alphabet.length)]);
         }
 
@@ -35,7 +35,6 @@ public class StringEvolution {
             for (int i = 0; i < length; i++) {
 
                 if (currentString.charAt(i) != targetString.charAt(i)) {
-                    Random randomIndex = new Random();
                     currentString = replaceIndex(currentString, i, alphabet[randomIndex.nextInt(alphabet.length)]);
                 }
             }
